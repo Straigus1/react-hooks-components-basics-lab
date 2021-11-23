0ecd77f7ca24dfb8ffe695d527bb8babca4749a9
Dayton <kirylmitch@gmail.com>
2021-11-23 09:32:20 -0500

    commit tree a3d8b23d157cdc83d9b85100eef756cc6a5b1001

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,40 @@
 import React from "react";
+import NavBar from "./NavBar";
+import Home from "./Home";
+import About from "./About";
 
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
-  );
-}
+// function NavBar() {
+//   return (
+//     <nav>
+//       <a href="#home">I'm a link!</a>
+//     </nav>
+//   );
+// }
 
-function Home() {
-  return (
-    <div id="home">
-      <h1>Home</h1>
-    </div>
-  );
-}
+// function Home() {
+//   return (
+//     <div id="home">
+//       <h1>Home</h1>
+//     </div>
+//   );
+// }
 
-{/* write an <About> component here */}
+// {/* write an <About> component here */
+// function About() {
+//   return (
+//     <div id="about">
+//       <h2>About</h2>
+//     </div>
+//   )
+// }}
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
